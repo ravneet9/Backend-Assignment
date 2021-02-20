f3634c1586dfade190c0d34122ae4205e71c8cce
Ravneet kaur <rvkaurrv9@gmail.com>
2021-02-21 00:48:35 +0530

    Add files via upload

---
+++ UserException.java
@@ -0,0 +1,11 @@
+package newProject;
+
+public class UserException extends Exception{
+    String message;
+    UserException(String str){
+        message=str;
+    }
+    public String toString() {
+      return ("Custom Exception Occurred : " + message);
+   }
+}
